a5746cf507ab70f5b6c263fc94fdd15c2fc1e36c
YomnaAlaaDarwish <yomna.ad2002@gmail.com>
2022-12-31 18:15:13 +0200

    payment Api

--- FawrySystemApi/src/main/java/FCAI/FawrySystemApi/Transaction/TransactionEndPoint.java
+++ FawrySystemApi/src/main/java/FCAI/FawrySystemApi/Transaction/TransactionEndPoint.java
@@ -4,16 +4,33 @@ import java.util.Vector;
 
 import org.springframework.web.bind.annotation.*;
 
-import FCAI.FawrySystemApi.Services.Service;
+import FCAI.FawrySystemApi.Services.*;
 
 @RestController
 public class TransactionEndPoint {
-	TransactionProcess transactionProcess=new TransactionProcess();
+	HandelPaymentProcess handel=new HandelPaymentProcess();
 	@GetMapping("/showAllServices")
-	public Vector<Service> add_wallet( )
+	public Vector<Service> showAllServices()
 	{
-		return transactionProcess.get_services();
+		return handel.getService();
 	}
+	@GetMapping("/showPaymentMethod/{service}/{provider}")
+	public String showPaymentMethod(@PathVariable("service") String service,@PathVariable("provider") String provider)
+	{
+		return handel.showPayment(service, provider);
+	}
+	@GetMapping("/getForm/{id}/{service}/{provider}/{method}")
+	public Vector<String> getForm(@PathVariable("id") int id,@PathVariable("service") String service,@PathVariable("provider") String provider,@PathVariable("method") String method)
+	{
+		return handel.getForm(id,service, provider,method);
+	}
+	@PostMapping("/pay/{id}/{service}/{provider}/{method}")
+	public Vector<String> pay(@PathVariable("id") int id,@PathVariable("service") String service,@PathVariable("provider") String provider,@PathVariable("method") String method,@RequestBody Vector<String>answer)
+	{
+		return handel.pay(id,service, provider,method,answer);
+	}
+	
+	
 	
 
 }
